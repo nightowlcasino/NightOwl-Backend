44e5c4a6b10f8874620d7bc544137b63139f9e47
supremedacos <nightowlcasino@protonmail.com>
2022-10-14 18:07:04 -0400

    fix error handling

--- src/api/ergo.controller.ts
+++ src/api/ergo.controller.ts
@@ -9,18 +9,33 @@ const encodeHexUrl = "/api/v1/ergo/encode-hex"
 export default class ErgoNodeController {
   static async EncodeNum(req: Request, res: Response): Promise<void> {
     let encoded: string = ""
+    let body: any
     let isInt: boolean = false
 
+    // validate request input
     try {
-      if (req.body.isInt) {
-        isInt = JSON.parse(req.body.isInt.toLowerCase());
+      body = JSON.parse(JSON.stringify(req.body))
+      if (body.isInt) {
+        isInt = JSON.parse(body.isInt);
       }
-    } catch (e) { }
+    } catch (e) {
+      logger.debug({
+        code: 400,
+        message: 'encode number invalid request',
+        error: `${getErrorMessage(e)}`,
+        body: `${JSON.stringify(req.body)}`,
+        url: encodeNumUrl,
+      });
+
+      res.status(400).json({ result: -1, error: `${getErrorMessage(e)}` })
+      return
+    }
 
     try {
       encoded = await encodeNum(req.body.number.toString(), isInt)
     } catch (e) {
       logger.debug({
+        code: 500,
         message: 'encode number failed',
         error: `${getErrorMessage(e)}`,
         body: `${JSON.stringify(req.body)}`,
@@ -33,7 +48,7 @@ export default class ErgoNodeController {
 
     logger.debug({
       message: 'encoded number',
-      number: `${req.body.number}`,
+      number: `${body.number}`,
       encoded: `${encoded}`,
       url: encodeNumUrl,
     });
@@ -44,11 +59,29 @@ export default class ErgoNodeController {
 
   static async EncodeHex(req: Request, res: Response): Promise<void> {
     let encoded: string = ""
+    let body: any
 
+    // validate request input
     try {
-      encoded = await encodeHex(req.body.reg.toString())
+      body = JSON.parse(JSON.stringify(req.body))
     } catch (e) {
       logger.debug({
+        code: 400,
+        message: 'encode hex invalid request',
+        error: `${getErrorMessage(e)}`,
+        body: `${JSON.stringify(req.body)}`,
+        url: encodeNumUrl,
+      });
+
+      res.status(400).json({ result: -1, error: `${getErrorMessage(e)}` })
+      return
+    }
+
+    try {
+      encoded = await encodeHex(body.reg.toString())
+    } catch (e) {
+      logger.error({
+        code: 500,
         message: 'encode hex failed',
         error: `${getErrorMessage(e)}`,
         body: `${JSON.stringify(req.body)}`,
@@ -61,7 +94,7 @@ export default class ErgoNodeController {
 
     logger.debug({
       message: 'encoded hex',
-      reg: `${req.body.reg}`,
+      reg: `${body.reg}`,
       encoded: `${encoded}`,
       url: encodeHexUrl,
     });
