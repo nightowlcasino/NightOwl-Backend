c1a0bd9aeac9b07233669218279a89616dbf1119
supremedacos <nightowlcasino@protonmail.com>
2022-10-14 20:45:32 -0400

    a more finalized version of max-payout, still need to calculate pending bet txs

--- src/models/liquidity.ts
+++ src/models/liquidity.ts
@@ -1,19 +1,87 @@
+import { TOKENID_FAKE_OWL, TOKENID_FAKE_LP } from "../constants/ergo"
 
 export class Liquidity {
 
-  public amount: number
+  public owlAmount: number = 0
+  public lpAmount?: number
+  public nanoErgs?: number
+  public tokens?: Array<Token>
 
   constructor(
-    amount: number,
+    owlAmount: number,
+    lpAmount?: number,
+    nanoErgs?: number,
+    tokens?: Array<Token>
   ) {
-    this.amount = amount
+    this.owlAmount = owlAmount
+    this.lpAmount = lpAmount
+    this.nanoErgs = nanoErgs
+    this.tokens = tokens
   }
 
-  public static fromJson(userJson: any): Liquidity {
+  public static fromJson(json: any): Liquidity {
+    let tokens = new Array<Token>()
+    let owlAmnt: number = 0
+    let lpAmnt: number = 0
+    // construct Array<Token> first
+    json.tokens.forEach((token: any) => {
+      tokens.push(new Token(
+        token.tokenId,
+        token.amount,
+        token.decimals,
+        token.name,
+        token.tokenType
+      ))
+
+      if (token.tokenId === TOKENID_FAKE_OWL) {
+        owlAmnt = token.amount
+      } else if (token.tokenId === TOKENID_FAKE_LP) {
+        lpAmnt = token.amount
+      }
+    })
+
     var lq = new Liquidity(
-      Number(userJson.amount),
+      owlAmnt,
+      lpAmnt,
+      Number(json.nanoErgs),
+      tokens
     );
 
     return lq;
+  }
+}
+
+export class Token {
+
+  public tokenId: string
+  public amount: number
+  public decimals: number
+  public name: string
+  public tokenType: string
+
+  constructor(
+    tokenId: string,
+    amount: number,
+    decimals: number,
+    name: string,
+    tokenType: string
+  ) {
+    this.tokenId = tokenId
+    this.amount = amount
+    this.decimals = decimals
+    this.name = name
+    this.tokenType = tokenType
+  }
+
+  public static fromJson(json: any): Token {
+    var token = new Token(
+      json.tokenId,
+      Number(json.amount),
+      Number(json.decimals),
+      json.name,
+      json.tokenType
+    );
+
+    return token;
   }
 }
--- src/workers/max-payout.ts
+++ src/workers/max-payout.ts
@@ -2,17 +2,14 @@ import { Observable, Subject } from "threads/observable"
 import { expose } from "threads/worker"
 import redisClient from "../redis/redis"
 import { get } from '../utils/rest'
-import { currentHeight } from '../ergo/explorer'
 import { getErrorMessage } from '../utils/error'
 import { Liquidity } from "../models/liquidity"
-import { TOKENID_FAKE_LP, TEST_HOUSE_CONTRACT_ADDRESS } from '../constants/ergo'
+import { TEST_HOUSE_CONTRACT_ADDRESS } from '../constants/ergo'
 
 const explorerApi = "https://api.ergoplatform.com/api/v1"
 //const explorerApi = "https://ergo-explorer-cdn.getblok.io/api/v1"
-const limit = 50
 const redisLiquidityTotalKey = "liquidity:total"
 const redisLiquidityPendingKey = "liquidity:pending"
-const redisLiquidityLastHeightKey = "liquidity:lastHeight"
 
 const sleep = async (durationMs: number) => {
   return new Promise(resolve => setTimeout(resolve, durationMs));
@@ -26,29 +23,35 @@ const maxPayoutWorker = {
     for (; ;) {
       subject.next("max-payout task started")
 
-      let lastHeight = await getLastHeight()
-      let currHeight = await currentHeight()
       const liquidity = new Liquidity(0)
 
       let url = `/addresses/${TEST_HOUSE_CONTRACT_ADDRESS}/balance/confirmed?minConfirmations=3`
       const batch = await get(explorerApi + url)
-        .then(resp => {
-          console.log(resp)
+        .then(async resp => {
+          const tokens = JSON.parse(JSON.stringify(resp))
+          const liquidityAmount: Liquidity = Liquidity.fromJson(tokens)
+
+          subject.next({
+            message: "successfully got liquidity pool tokens",
+            owl_token_amount: liquidityAmount.owlAmount,
+            lp_token_amount: liquidityAmount.lpAmount,
+            max_payout: liquidityAmount.owlAmount * 0.15
+          })
+
+          // save off latest LP OWL balance which we capture every 30 seconds
+          const res = await redisClient.set(redisLiquidityTotalKey, liquidityAmount.owlAmount || 0)
+          if (res !== "OK") {
+            subject.error(`failed to write ${redisLiquidityTotalKey} to redis db`)
+          }
         })
         .catch(err => {
           subject.error(getErrorMessage(err))
         })
 
-      // save off current height as last height
-      //const res = await redisClient.set(redisLeaderBrdLastHeightKey, currHeight)
-      //if (res !== "OK") {
-      //  subject.error(`failed to write ${redisLeaderBrdLastHeightKey} to redis db`)
-      //}
+      subject.next("max-payout task finished")
 
-      subject.next("leaderboard task finished")
-
-      // sleep for 2min
-      await sleep(120000)
+      // sleep for 30 seconds
+      await sleep(30000)
     }
 
   },
@@ -60,22 +63,6 @@ const maxPayoutWorker = {
     return Observable.from(subject)
   }
 };
-
-async function getLastHeight() {
-  let height = 0
-  let res = await redisClient.get(redisLiquidityLastHeightKey);
-  if (res === null) {
-    // default to 0 if missing
-    res = await redisClient.set(redisLiquidityLastHeightKey, 0)
-    if (res !== "OK") {
-      subject.error(`failed to write ${redisLiquidityLastHeightKey} to redis db`)
-    }
-  } else {
-    height = Number(res)
-  }
-
-  return height
-}
 
 export type MPWorker = typeof maxPayoutWorker
 
