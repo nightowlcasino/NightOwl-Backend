bcbf44ccfb4c6921e4dd850f41530aa6688ccabc
supremedacos <nightowlcasino@protonmail.com>
2022-10-17 10:49:09 -0400

    Add api call to convert address to ergotree base16

--- src/api/ergo.controller.ts
+++ src/api/ergo.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express"
 import { encodeNum, encodeHex } from "../ergo/serializer"
+import { Address } from '../../pkg-nodejs/ergo_lib_wasm'
 import { getErrorMessage } from '../utils/error'
 import logger from "../logger"
 
 const encodeNumUrl = "/api/v1/ergo/encode-num"
 const encodeHexUrl = "/api/v1/ergo/encode-hex"
+const ergoTreeBase16Url = "/api/v1/ergo/ergo-tree-base16"
 
 export default class ErgoNodeController {
   static async EncodeNum(req: Request, res: Response): Promise<void> {
@@ -78,7 +80,7 @@ export default class ErgoNodeController {
         message: 'encode hex invalid request',
         error: `${err}`,
         body: `${bodyRaw}`,
-        url: encodeNumUrl,
+        url: encodeHexUrl,
       });
 
       res.status(400).json({ result: -1, error: `${err}` })
@@ -110,6 +112,57 @@ export default class ErgoNodeController {
     });
 
     res.status(200).json({ result: `${encoded}`, error: null })
+    return
+  }
+
+  static async ErgoTreeBase16(req: Request, res: Response): Promise<void> {
+    let base16: string = ""
+    let body: any
+    let bodyRaw: string = ""
+
+    // validate request input
+    try {
+      bodyRaw = JSON.stringify(req.body)
+      body = JSON.parse(bodyRaw)
+    } catch (e) {
+      const err = getErrorMessage(e)
+      logger.error({
+        code: 400,
+        message: 'convert address to ergo tree base16 invalid request',
+        error: `${err}`,
+        body: `${bodyRaw}`,
+        url: ergoTreeBase16Url,
+      });
+
+      res.status(400).json({ result: -1, error: `${err}` })
+      return
+    }
+
+    try {
+      base16 = Address.from_mainnet_str(body.addr.toString()).to_ergo_tree().to_base16_bytes()
+    } catch (e) {
+      const err = getErrorMessage(e)
+      logger.error({
+        code: 500,
+        message: 'convertion from address to ergotree base16 failed',
+        error: `${err}`,
+        body: `${bodyRaw}`,
+        url: ergoTreeBase16Url,
+      });
+
+      res.status(500).json({ result: -1, error: `${err}` })
+      return
+    }
+
+    logger.debug({
+      code: 200,
+      message: 'converted address to ergotree base16',
+      address: `${body.addr}`,
+      ergoTreeBase16: `${base16}`,
+      url: ergoTreeBase16Url,
+    });
+
+    res.status(200).json({ result: `${base16}`, error: null })
     return
   }
 }
--- src/api/ergo.route.ts
+++ src/api/ergo.route.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route("/encode-num").post(ErgoController.EncodeNum)
 router.route("/encode-hex").post(ErgoController.EncodeHex)
+router.route("/ergo-tree-base16").post(ErgoController.ErgoTreeBase16)
 
 export default router
